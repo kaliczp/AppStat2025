dd94f113c12e4f18c8431934e71a2ed9948b0566
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2025-09-30 08:36:38 +0200

    Integrate last class

--- FirstScript.R
+++ FirstScript.R
@@ -1,1 +1,21 @@
 ### Introduction to R environment
+## 1. Start R
+## 2. Create an empty folder (possible a direct path (in C: )
+## 3. getwd() # Hit Enter! And it gives the working folder
+getwd()
+## [1] "/home/kaliczp"
+## Use up arrow to recall the former commands.
+## Watch prompt >
+## 4. Usage of functions.
+##   In the case you forget braces.
+##   Use up arrow and clear both braces!
+##   If you type in an object name and hit enter
+##   you can see the content of the object.
+## 5. Unfinished expression. The prompt changed to „+”
+## 6. citation() # try to see the code of the function!
+citation()
+## 7. Set the working folder. Interactive way:
+##  File → Change dir … alternative use setwd()
+##  Check it!
+## 8. Quit R with q()
+## 9. Look at the folder!
