56a568386cabdf3a7b265acab2bb964dcb9f5284
Péter Kalicz <kaliczp@gmail.com>
2025-10-07 10:55:17 +0200

    assignment an man pages

--- Object.R
+++ Object.R
@@ -1,6 +1,15 @@
 ## Create an objet called a and put 1 into it
 a = 1
+## Official is < and - the same operator
+a <- 1
 ## look into the object
 a
 ## List of objects
 ls()
+## Our second object
+b <- 2
+## Combine objects
+my_con <- c(a, b)
+## Manual pages, every funciton and system part has a manual page
+?c # man. page for c() function
+?ls # man. pag for ls()
