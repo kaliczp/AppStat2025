3e7e4f9e1beda1d89843066ef495e7f9afcfce76
Péter Kalicz <kaliczp@gmail.com>
2025-10-14 11:45:49 +0200

    indexing and random normal numbers

--- Object.R
+++ Object.R
@@ -36,6 +36,15 @@ seq(-3, 300, 3) # sequence as above but increased by 3
 ?seq
 help(seq)
 seq() # there are default values for arguments
+seq(to = 10) # you can use any argument with name in any order
+seq(TO = 10) # R is case-sensitive you need carefully use up and down cases
+seq(to = 10, from = 1) # you can give in any order arguments with name
+seq(10 ,1) # If you did not give a name these are interperted in the default order see in manual page
+seq.int(1,10,1) # for integers (whole numbers)
+args(seq.int) # gives arguments list besides man. page. sometimes no result in like args(seq) 
+seq(1, 10, by = 0.5) # seq works with real numbers
+
+## Different types
 NULL # Nothing mainly in programming
 NA # nothin or Not Available in data
 NaN # not a number, result of calulatin
@@ -48,3 +57,23 @@ a > 100
 ls() # before you contiune your work let's check existing objects
 ## Character type
 month.name # part of the system, there are many object in R
+?month.name
+"An exaple"
+'Another example' # You can use any kind of apostrophes
+fruits <- c("apple", "orange", "banana")
+
+## Indexing and vectorised execution
+my_vector[2] # second member of the vector
+my_vector[2:4] # elements from 2 until 4
+my_vector[c(1,3,5)] # First, 3rd and 5th elements
+## Vectorised investigation
+my_vector > 5 # elements bigger then 5
+my_vector + 5 # add 5 to all elements
+## Indexing with logical expression
+my_vector[my_vector > 5] # Elements bigger then 5
+
+## Random numerical vector with normal distribution
+rnorm(10) # 10 numbers follw normal distribution
+?rnorm # man. page
+my_norm <- rnorm(20)
+my_norm[my_norm >= 0]
