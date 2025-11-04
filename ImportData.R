13e58a3ba089c77bcb7279b4945a92d16544d0f5
Péter Kalicz <kaliczp@gmail.com>
2025-11-04 11:48:09 +0100

    Histogram and not normal distribution precipitation

---
+++ ImportData.R
@@ -0,0 +1,15 @@
+dir() # Control your dataset is available
+help.start() # There is an import/export manual
+read.table("r_h_Budapest_18702023.csv") # Drops an error
+BpRain <- read.table("r_h_Budapest_18702023.csv", sep = ";") # with separator declare
+str(BpRain)
+BpRain <- read.table("r_h_Budapest_18702023.csv", sep = ";", header = TRUE) # with separator declare
+
+summary(BpRain$r)
+boxplot(BpRain$r)
+
+## Index the values greater than zero
+BpRainyDays <- BpRain[BpRain$r > 0, "r"]
+boxplot(BpRainyDays)
+
+## Histogram and density
--- NormalDistribution.R
+++ NormalDistribution.R
@@ -4,3 +4,11 @@ median(my_norm) # median calculation
 mean(my_norm) # mean calculation
 
 set.seed(10) # set the initial state of random number genration
+
+hist(my_norm) # Histogram
+boxplot(my_norm) # Boxplot
+summary(my_norm) # Summary Tkey five numbers + mean
+
+my_ext <- c(my_norm,100)
+summary(my_ext)
+boxplot(my_ext)
