c8dda5f71492ff86f726bad4ecd9a05a9dd93dd7
Péter Kalicz <kaliczp@gmail.com>
2025-11-04 11:03:07 +0100

    Set seed

--- NormalDistribution.R
+++ NormalDistribution.R
@@ -1,2 +1,6 @@
 my_norm <- rnorm(100) # 100 generated standard normal distr. data
 quantile(my_norm) # Calculate quantiles
+median(my_norm) # median calculation
+mean(my_norm) # mean calculation
+
+set.seed(10) # set the initial state of random number genration
