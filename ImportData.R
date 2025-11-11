9a0c611da3c33d1b558fcf09085d24b128d42e91
Péter Kalicz <kaliczp@gmail.com>
2025-11-11 12:48:37 +0100

    Weekly colored precipitation and plan

--- ImportData.R
+++ ImportData.R
@@ -16,3 +16,8 @@ boxplot(BpRainyDays)
 ## Histogram and density
 hist(BpRainyDays)
 barplot(BpRainyDays)
+
+## Barplot for the first year weekly changing color
+barplot(BpRainyDays[1:365], col = c(rep("#e0ecf4",7),rep("#8856a7",7)), xaxs = "i")
+
+## Linear regression
