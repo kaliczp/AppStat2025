8703ab41f5ae1531e5a440cc1b3a0546bb7d3cfc
Péter Kalicz <kaliczp@gmail.com>
2025-11-18 11:56:08 +0100

    some comments

--- ImportData.R
+++ ImportData.R
@@ -25,12 +25,12 @@ barplot(BpRainyDays)
 barplot(BpRain[1:365, "r"], col = c(rep("#e0ecf4",7),rep("#8856a7",7)), xaxs = "i") # graph parameters from ?par
 
 ## Linear regression
-head(BpRain)
-BpRain$Index <- 1:nrow(BpRain)
-BpRain.lm <- lm(r ~ Index, BpRain)
-BpRain.lm
-str(BpRain.lm)
-summary(BpRain.lm)
+head(BpRain) # first lines in data.frame
+BpRain$Index <- 1:nrow(BpRain) # generate index column
+BpRain.lm <- lm(r ~ Index, BpRain) # ?lm read about linear model (regression) in manual page
+BpRain.lm # See the object reallly simple output Index shows decreasing in precipitation
+str(BpRain.lm) # but it is a complicatbed object
+summary(BpRain.lm) # summary is our friend
 ## Significant, but not meaningful!
 BpRain.lm$coefficients[2]*36500
 
