981d3aa97d05532200209530be51535c1b373fdc
Péter Kalicz <kaliczp@gmail.com>
2025-11-25 10:30:03 +0100

    Regression anal

--- TimeSeries.R
+++ TimeSeries.R
@@ -1,2 +1,12 @@
 ## Clean import of dataset
 BpRain <- read.table("r_h_Budapest_18702023.csv", sep = ";", header = TRUE)
+
+## Linear regression based on generated Index
+BpRain$Index <- 1:nrow(BpRain) # generate index column
+BpRain.lm <- lm(r ~ Index, BpRain) # ?lm read about linear model (regression) in manual page
+?lm # do not forget the manual page
+summary(BpRain.lm) # main statistics about regression
+
+## Visualise
+plot(r ~ Index, BpRain, type = "h", xaxs = "i")
+abline(BpRain.lm, col = "red")
